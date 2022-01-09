5febf421b0928dc5174432e38126b1b1d57886a5
20547231Kav <kavinsau21@gmail.com>
2022-01-09 12:35:36 +0530

    initial commmit

---
+++ src/test/java/com/assesment/grading_tool/GradingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.grading_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
