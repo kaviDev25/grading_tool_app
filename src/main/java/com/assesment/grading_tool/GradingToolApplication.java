4ec3e00fb0d8b70293321f36f7cb05fd5749ff6d
20547231Kav <kavinsau21@gmail.com>
2022-01-10 17:14:53 +0530

    final commit

--- src/main/java/com/assesment/grading_tool/GradingToolApplication.java
+++ src/main/java/com/assesment/grading_tool/GradingToolApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
